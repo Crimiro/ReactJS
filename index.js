70d4f17cec6d2f8b745e7a73b34de45d40e6e173
Crimiro <crimiro05@gmail.com>
2018-10-28 21:42:55 -0600

    Add styles and proptypes

--- index.js
+++ index.js
@@ -4,4 +4,4 @@ import Media from './src/playlist/components/media';
 
 const app = document.getElementById('app')
 const holaMundo = <h1>Hola mundo!</h1>;
-ReactDOM.render(<Media/>, app);
+ReactDOM.render(<Media type='audio' title='¿Qué es Responsive Design?' author='Christian Rosales' image='./images/covers/responsive.jpg'/>, app);
--- src/playlist/components/media.js
+++ src/playlist/components/media.js
@@ -1,22 +1,32 @@
-import React from 'react';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
-class Media extends React.Component {
+import './media.css';
+class Media extends Component {
 	render() {
 		return(
-			<div>
-				<div>
+			<div className='Media'>
+				<div className='Media-cover'>
 					<img 
-						src=''
+						className='Media-image'
+						src={this.props.image}
 						alt=''
 						width={260}
 						height={160}
 					/>
-					<h3>¿Por qué aprender React?</h3>
-					<p>Christian Rosales</p>
+					<h3 className='Media-title'>{this.props.title}</h3>
+					<p className='Media-author'>{this.props.author}</p>
 				</div>
 			</div>			
 		);
 	}
+}
+
+Media.propTypes = {
+	image : PropTypes.string,
+	title : PropTypes.string.isRequeried,
+	author : PropTypes.string,
+	type : PropTypes.oneOf(['video', 'audio'])
 }
 
 export default Media;
