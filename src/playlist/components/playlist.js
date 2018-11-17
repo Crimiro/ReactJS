6f268e48914ca7507211e7ab00f72c6f83447e9a
Crimiro <crimiro05@gmail.com>
2018-11-16 18:50:02 -0600

    composición de componentes

--- src/playlist/components/playlist.js
+++ src/playlist/components/playlist.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import Media from './media';
 import './playlist.css';
+import Play from '../../icons/components/play';
+import Pause from '../../icons/components/pause';
+import Volume from '../../icons/components/volume';
+import FullScreen from '../../icons/components/full-screen';
 
 function Playlist(props) {
 		return (
 			<div>
+				<Play size={50} color='red'/>
+				<Pause size={50} color='red'/>
+				<Volume size={50} color='red'/>
+				<FullScreen size={50} color='red'/>
 				{props.data.categories.map( (category) => {
 					return (
 						<div key={category.id}>
